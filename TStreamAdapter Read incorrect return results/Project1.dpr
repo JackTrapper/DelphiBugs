85efb92f304ff4e4c81f2271ab4e57c632e1e9fa
JackTrapper <josejimeniz+github@gmail.com>
2022-02-19 13:34:10 -0500

    Update Project1.dpr

--- TStreamAdapter Read incorrect return results/Project1.dpr
+++ TStreamAdapter Read incorrect return results/Project1.dpr
@@ -27,10 +27,20 @@ begin
 	stm := System.Classes.TStreamAdapter.Create(stream, soOwned);
 
 	hr := stm.Read(@buffer, 1, @bytesRead);
+
+	WriteLn('Bytes requeste to read: '+IntToStr(1));
+	WriteLn('Bytes actually read: '+IntToStr(bytesRead));
+	WriteLn(Format('HRESULT: %.8x', [hr]));
+
 	if bytesRead < 1 then
 	begin
 		if hr <> S_FALSE then
-			raise Exception.CreateFmt('TStreamAdapter test failed. Desired: %d. Read: %d. HRESULT: %.8x', [1, bytesRead, hr]);
+		begin
+			if hr = S_OK then
+				raise Exception.Create('TStreamAdapter test failed. When read fewer bytes than requested, the correct HRESULT is S_FALSE, but instead was S_OK.')
+			else
+				raise Exception.CreateFmt('TStreamAdapter test failed. When read fewer bytes than requested, the correct HRESULT is S_FALSE, but instead was %.8x.', [hr]);
+      end;
 	end;
 end;
 
@@ -41,4 +51,6 @@ begin
     on E: Exception do
       Writeln(E.ClassName, ': ', E.Message);
   end;
+	WriteLn('Press enter to close...');
+   ReadLn;
 end.
